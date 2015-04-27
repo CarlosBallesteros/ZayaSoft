3908a821fafc9079ffb25e740019e861c81de4f4
Carlos <soraksolrak@gmail.com>
2015-04-27 13:12:23 +0200

    1.7
    Signed-off-by: Carlos <soraksolrak@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		int a,b;
+		int n=1;
 		Scanner leer=new Scanner(System.in);
-		System.out.println("¿Que operacion desea realizar?\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir");
-		switch(leer.nextInt()){
+		while(n!=0){
+		System.out.println("¿Que operacion desea realizar?\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir\n0.Terminar");
+		n=leer.nextInt();
+		switch(n){
 		case 1:System.out.println("Introduce el primer numero:");
 			a=leer.nextInt();
 			System.out.println("Introduce el segundo numero:");
@@ -32,7 +35,7 @@ public class Main {
 		}
 		
 		
-		
+		}
 		
 	}
 

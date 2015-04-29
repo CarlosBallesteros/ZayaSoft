bb1c75c5ccdad35d33c9284b628a51396bdfa5e5
Carlos <soraksolrak@gmail.com>
2015-04-29 13:12:49 +0200

    3.0 Calculadora completa
    Signed-off-by: Carlos <soraksolrak@gmail.com>

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,16 +1,26 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 public class Ventana extends JFrame{
+	//Parametros de los paneles
 	JPanel panel_principal;
 	JPanel panel_secundario;
-	JTextField panel_numerico;
+	JTextField caja;
+	//Parametros de los botones
+	JButton boton[];
+	double op1=0,op2=0,memoria=0;
+	String operacion="";
+	boolean nueva=true;
+	
 	
 	//Metodo main (opcional?)
 	public static void main(String[] args) {
@@ -21,20 +31,251 @@ public class Ventana extends JFrame{
 	private void init() {
 		panel_principal=new JPanel();
 		panel_secundario=new JPanel();
-		panel_numerico=new JTextField();
+		caja=new JTextField();
 		panel_principal.setLayout(new BorderLayout());
-		panel_principal.add("North",panel_numerico);
-		panel_secundario.setLayout(new GridLayout(5,4,8,8));
+		panel_principal.add("North",caja);
+		panel_secundario.setLayout(new GridLayout(5,4,5,5));
+		añadirBotones();
 		panel_principal.add("Center",panel_secundario);
 		getContentPane().add(panel_principal);
+	}
+	
+	//Crear los botones
+	private void añadirBotones() {
+		boton = new JButton[20];
+		
+        //INICIALIZAMOS LOS BOTONES
+        boton[0]=new JButton("CE");
+        boton[1]=new JButton("MR");
+        boton[2]=new JButton("M+");
+        boton[3]=new JButton("M-");
+        boton[4]=new JButton("7");
+        boton[5]=new JButton("8");
+        boton[6]=new JButton("9");
+        boton[7]=new JButton("/");
+        boton[8]=new JButton("4");
+        boton[9]=new JButton("5");
+        boton[10]=new JButton("6");
+        boton[11]=new JButton("*");
+        boton[12]=new JButton("1");
+        boton[13]=new JButton("2");
+        boton[14]=new JButton("3");
+        boton[15]=new JButton("-");
+        boton[16]=new JButton("0");
+        boton[17]=new JButton(".");
+        boton[18]=new JButton("=");
+        boton[19]=new JButton("+");
+        //AGREAMOS LOS BOTONES AL PANEL BOTONES
+        for(int i=0;i<20;i++){
+            panel_secundario.add(boton[i]);
+        }
+        caja.setText("0");
+        //EVENTOS DE LOS BOTONES
+        //MEMORIA
+        boton[1].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                caja.setText("");
+                caja.setText(caja.getText()+memoria);
+                nueva=true;
+            }
+        });
+        boton[2].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+            	memoria=memoria+Double.parseDouble(caja.getText());
+            }
+        });
+        boton[3].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+            	memoria=memoria-Double.parseDouble(caja.getText());
+            }
+        });
+        //OPERACIONES
+        boton[19].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                try{
+                    if(op1!=0)
+                        op1=op1+Double.parseDouble(caja.getText());
+                    else
+                        op1=Double.parseDouble(caja.getText());
+                    operacion="suma";
+                    //caja.setText(caja.getText()+"+");
+                    nueva=true;
+                }catch(Exception err){}
+            }
+        });
+        boton[15].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                try{
+                    if(op1!=0)
+                        op1=op1-Double.parseDouble(caja.getText());
+                    else
+                        op1=Double.parseDouble(caja.getText());
+                    operacion="resta";
+                    //caja.setText(caja.getText()+"-");
+                    nueva=true;
+                }catch(Exception err){}
+            }
+        });
+        boton[11].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                try{
+                    if(op1!=0)
+                        op1=op1*Double.parseDouble(caja.getText());
+                    else
+                        op1=Double.parseDouble(caja.getText());
+                    operacion="multiplicacion";
+                    //caja.setText(caja.getText()+"*");
+                    nueva=true;
+                }catch(Exception err){}
+            }
+        });
+        boton[7].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                try{
+                    if(op1!=0)
+                        op1=op1/Double.parseDouble(caja.getText());
+                    else
+                        op1=Double.parseDouble(caja.getText());
+                    operacion="division";
+                    //caja.setText(caja.getText()+"/");
+                    nueva=true;
+                }catch(Exception err){}
+            }
+        });
+        //NUMEROS Y PUNTO DECIMAL
+        boton[4].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"7");
+            }
+        });
+        boton[5].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"8");
+            }
+        });
+        boton[6].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"9");
+            }
+        });
+        boton[8].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"4");
+            }
+        });
+        boton[9].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"5");
+            }
+        });
+        boton[10].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"6");
+            }
+        });
+        boton[12].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"1");
+            }
+        });
+        boton[13].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"2");
+            }
+        });
+        boton[14].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"3");
+            }
+        });
+        boton[16].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+"0");
+            }
+        });
+        boton[17].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                if(nueva){caja.setText("");nueva=false;}
+                caja.setText(caja.getText()+".");
+            }
+        });
+        //IGUAL
+        boton[18].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                try{
+                    op2=Double.parseDouble(caja.getText());
+                }catch(Exception err){}
+                if(operacion.equals("suma")){
+                    double res=op1+op2;
+                    caja.setText(String.valueOf(res));
+                    op1=op2=0;
+                    operacion="";
+                }else if(operacion.equals("resta")){
+                    double res=op1-op2;
+                    caja.setText(String.valueOf(res));
+                    op1=op2=0;
+                    operacion="";
+                }else if(operacion.equals("multiplicacion")){
+                    double res=op1*op2;
+                    caja.setText(String.valueOf(res));
+                    op1=op2=0;
+                    operacion="";
+                }else if(operacion.equals("division")){
+                    double res=op1/op2;
+                    caja.setText(String.valueOf(res));
+                    op1=op2=0;
+                    operacion="";
+                }
+                nueva=true;
+            }
+        });
+        //CE
+        boton[0].addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent evt){
+                caja.setText("0");
+                op1=op2=0;
+                operacion="";
+            }
+        });
+
 	}
 	
 	//Crear la ventana
 	public Ventana() {
 		setTitle("Calculadora");
-		setSize(400,300);
+		setSize(250,250);
 		setResizable(false);
-		setBounds(200, 200, 400, 300);
+		setBounds(250, 250, 250, 250);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		init();
 		setVisible(true);
